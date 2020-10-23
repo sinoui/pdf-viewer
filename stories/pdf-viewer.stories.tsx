b31b787129ebdac1a7dec54ddaa9d15dcf18a5ed
tianyanqiu <745916729@qq.com>
2020-10-23 16:47:59 +0800

    chore: 恢复尺寸调整

--- stories/pdf-viewer.stories.tsx
+++ stories/pdf-viewer.stories.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import test from './test.pdf';
 import PdfViewer from '../src';
+import './index.css';
 
 storiesOf('pdf-viewer', module).add('查看pdf', () => (
   <PdfViewer url={test} creator="办公厅/张三" />

2d3d714b70b9420b87b34cbcddeb24a848d74fd9
tianyanqiu <745916729@qq.com>
2020-10-25 15:02:35 +0800

    chore: 调整线的实现方式

--- src/icons/Line.tsx
+++ src/icons/Line.tsx
@@ -19,20 +19,39 @@ export default function Line(props: Props) {
         width: endX + 100,
       }}
     >
-      <line
-        x1="0"
-        y1="0"
-        x2={endX}
-        y2="60"
-        style={{ stroke: 'yellow', strokeWidth: 2 }}
-      />
-      <line
-        x1="0"
-        y1="0"
-        x2={endX}
-        y2="20"
-        style={{ stroke: 'yellow', strokeWidth: 2 }}
-      />
+      <path d={`M${endX} 10 L0 0 L${endX} 30`} fill="#ffffe0" stroke="yellow" />
     </svg>
   );
 }
+
+// export default function Line(props: Props) {
+//   const { startPoint, targetPoint } = props;
+//   const offsetX = startPoint.x + 26;
+//   const offsetY = startPoint.y + 13;
+//   const endX = targetPoint.x - offsetX;
+//   return (
+//     <svg
+//       style={{
+//         position: 'absolute',
+//         top: offsetY,
+//         left: offsetX,
+//         width: endX + 100,
+//       }}
+//     >
+//       <line
+//         x1="0"
+//         y1="0"
+//         x2={endX}
+//         y2="30"
+//         style={{ stroke: 'yellow', strokeWidth: 2 }}
+//       />
+//       <line
+//         x1="0"
+//         y1="0"
+//         x2={endX}
+//         y2="10"
+//         style={{ stroke: 'yellow', strokeWidth: 2 }}
+//       />
+//     </svg>
+//   );
+// }

7b423163336a55e7f92551e73cbadca307640b50
刘进行 <jackingstarliu@gmail.com>
2020-10-25 19:51:03 +0800

    fix: 修复 storybook 频繁重新加载的缺陷

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 };
